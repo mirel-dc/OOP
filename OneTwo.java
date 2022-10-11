bde0d5520888bfb17a5dc709074321aca7afaceb
Mirel <valerymirel@mail.ru>
2022-10-11 23:48:09 +0500

    da

--- OneTwo.java
+++ OneTwo.java
@@ -4,12 +4,13 @@ public class OneTwo {
 
     public static String Sklon(int q) {
         String result;
-        int last;
+        int last, predlast;
 
+        predlast = q % 100;
         last = q % 10;
 
-        if (q == 11) {
-            result = q + "rubley";
+        if ((predlast == 11) || (predlast == 12) || (predlast == 13) || (predlast == 14)) {
+            result = q + " rubley";
         } else if (last == 1) {
             result = q + " rubl'";
         } else if ((last == 2) || (last == 3) || (last == 4)) {
@@ -23,6 +24,8 @@ public class OneTwo {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println("Vvedite kolvo rubley: ");
+
         int rubl = in.nextInt();
 
         String obj = Sklon(rubl);
